c536eea99fd2ad9e0b703ab364025986cf89fea9
benchtamayo22 <vanphillip.tamayo@lorma.edu>
2025-02-15 22:16:13 +0800

    Update MainCoffeeObject.java

--- src/main/java/org/btamayo/MainCoffeeObject.java
+++ src/main/java/org/btamayo/MainCoffeeObject.java
@@ -5,13 +5,11 @@ public class MainCoffeeObject {
         String[] flavorNotes1 = {"Chocolate", "Nutty"};
         CoffeeObject coffee1 = new CoffeeObject("Espresso", "Arabica", "Small", 3.99, "Dark", "Italy", false, 50, flavorNotes1, "Espresso");
 
-        // Calling methods of the first coffee object
         System.out.println("Coffee Name: " + coffee1.name);
         System.out.println("Is in stock: " + coffee1.checkStock());
         coffee1.addFlavor("Citrus");
         System.out.println("Updated Description: " + coffee1.describe());
 
-        // Creating and calling methods for the second coffee object
         String[] flavorNotes2 = {"Caramel", "Fruity"};
         CoffeeObject coffee2 = new CoffeeObject("Latte", "Robusta", "Large", 5.99, "Medium", "Brazil", true, 30, flavorNotes2, "Drip");
         System.out.println("Coffee Name: " + coffee2.name);
